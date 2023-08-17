97429594f6b319cbf205502262d9bbcec4cd8579
NullBite <me@nullbite.com>
2023-08-17 18:17:56 -0400

    Add title-fix wip (just the metadata header)

---
+++ title-fix.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        X to Twitter (title)
+// @namespace   https://github.com/Silveere/no-x
+// @match       https://*.twitter.com/*
+// @match       https://*.x.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @grant       none
+// @version     1.0
+// @author      NullBite
+// @description Replace "X" in the title with "Twitter"
+// @run-at      document-start
+//
+// ==/UserScript==
+
